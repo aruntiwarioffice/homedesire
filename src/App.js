8c1b30250a4180aa7b3d21af78383ed568e53da7
Arun Kumar Tiwari <aruntiwarioffice@gmail.com>
2023-01-09 20:10:40 +0530

    Modified rep

--- src/App.js
+++ src/App.js
@@ -1,65 +1,32 @@
-// import logo from './logo.svg';
-import './App.css';
-import './Style.css';
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-// import { Route, Routes, Link } from 'react-router-dom';
-import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
-import Dashboard from "./components/dashboard/Dashboard";
-import Help from "./components/dashboard/Help";
-import Users from "./components/dashboard/Users";
-import About from "./pages/common/About";
-import Contact from "./pages/common/Contact";
-import Home from "./pages/common/Home";
-import Error from "./pages/Error";
-import Enquiry from "./pages/common/Enquiry";
-import Customer from "./pages/common/Customer";
-import PropertyPage from "./pages/common/PropertyPage";
-import PartnerRegister from "./pages/common/PartnerRegister";
-import Login from "./user/Login";
-import Registration from "./user/Registration";
-import AddUser from "./user/Add";
-import UserDetails from "./user/Details";
-import UserEdit from "./user/Edit";
-import UserList from "./user/List";
-// import Dashboard from "./admin/Dashboard";
+import { useContext } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
+import Layout from './components/Layout/Layout';
+import Error from "./pages/common/Error";
+import UserProfile from './components/Profile/UserProfile';
+import AuthPage from './pages/common/AuthPage';
+import HomePage from './pages/common/HomePage';
+import AuthContext from './resources/auth-context';
 
-
-//Please do not code in this page by shaurabh
 function App() {
+  const authCtx = useContext(AuthContext);
+
   return (
-    <div className="App">
-      <Header />
-
+    <Layout>
       <Routes>
-
-        <Route exact path='/' element={<Home />} />
-        <Route exact path='/home' element={<Home />} />
-        <Route exact path='/about' element={<About />} />
-        <Route exact path='/contact' element={<Contact />} />
-        <Route exact path='/enquiry-now' element={<Enquiry />} />
-        <Route exact path='/partner-register' element={<PartnerRegister />} />
-        <Route exact path='/login' element={<Login />} />
-        <Route exact path='/registration' element={<Registration />} />
-        {/* <Route exact path='/dashboard' element={<Dashboard />} /> */}
-        <Route exact path='/customer' element={<Customer />} />
-        <Route exact path='/property' element={<PropertyPage />} />
-        <Route exact path='/add-user' element={<AddUser />} />
-        <Route exact path='/user-details' element={<UserDetails />} />
-        <Route exact path='/edit-user' element={<UserEdit />} />
-        <Route exact path='/user-list' element={<UserList />} />
-        <Route exact path='/dashboard' element={<Dashboard />} />
-        <Route exact path='/help' element={<Help />} />
-        <Route exact path='/users' element={<Users />} />
+        <Route exact path='/' element={<HomePage />} />
+        {!authCtx.isLoggedIn && (
+          <Route exact path='/auth' element={<AuthPage />} />
+        )}
+        {authCtx.isLoggedIn && (
+          <Route exact path='/auth' element={<UserProfile />} />
+        )}
+        {!authCtx.isLoggedIn && (
+          <Route exact path='/auth' element={<AuthPage />} />
+        )}
         <Route path="*" element={<Error />} />
-
-
       </Routes>
-
-      <Footer />
-    </div>
+    </Layout>
   );
 }
 
